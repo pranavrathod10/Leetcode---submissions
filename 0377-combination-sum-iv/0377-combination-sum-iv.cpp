36fb78f8504a561c21aafda080f42ae59ce24ef3
Pranav Rathod <76491743+pranavrathod10@users.noreply.github.com>
2026-01-09 02:06:39 +0530

    Time: 4 ms (29.53%), Space: 9.2 MB (47.36%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,27 +1,34 @@
 class Solution {
+    vector<int> memo;
 public:
-    int func(vector<int>& nums, int n, int tar,unordered_map<int,int>& memo){
-        if(memo.count(tar)>0){
-            return memo[tar];
-        }
-        if(tar == 0){
-            memo[tar] = 1;
+    int func(vector<int>& nums, int target){
+        // if(idx >= nums.size()){
+        //     return target == 0? 1: 0;
+        // }
+        // else if()
+        if(target == 0){
             return 1;
         }
         
+        if(memo[target] != -1){
+            return memo[target];
+        }
+
         int ans = 0;
-        for(int i=0;i<n;i++){
-            if(tar >= nums[i]){
-                ans += func(nums,n,tar - nums[i],memo);
+        for(int x: nums){
+            if(target - x >= 0){
+                ans += func(nums, target - x);
+                // ans += func(nums, target, idx+1);
             }
         }
-        memo[tar] = ans;
+        
+        memo[target] = ans;
         return ans;
-    }
-    int combinationSum4(vector<int>& nums, int tar) {
-        int n = nums.size();
-        unordered_map<int,int> memo;
-        return func(nums,n,tar,memo);
 
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        memo.resize(target+1,-1);
+        return func(nums,target);
     }
 };
